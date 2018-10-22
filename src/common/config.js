1ffe22014c002fd6f2d5e5a759c04f5f9d61e968
radsen81 <andreschennai@gmail.com>
2018-10-22 13:53:49 +0200

    backup

--- src/common/config.js
+++ src/common/config.js
@@ -2,5 +2,3 @@ export default {}
 export const API_URL = 'http://localhost:8082/portus/api';
 export const GOOGLE_API_KEY = '1234';
 export const BASE_URL_PORTUS = 'https://portus.puertos.es/Portus/';
-
-//export const API_URL = 'https://jsonplaceholder.typicode.com'
